9eac84bc179bb6160b123f1b5f9b1c3d47288390
QuwackJ <quwackj@hotmail.com>
2023-03-20 18:05:41 -0700

    Emptied class because I put code in the wrong place

--- src/main/java/io/github/quwackj/spiritedawayghs/entity/custom/DustBinCreature.java
+++ src/main/java/io/github/quwackj/spiritedawayghs/entity/custom/DustBinCreature.java
@@ -1,14 +1,5 @@
 package io.github.quwackj.spiritedawayghs.entity.custom;
 
-import net.minecraft.server.level.ServerLevel;
-import net.minecraft.world.entity.AgeableMob;
-import net.minecraft.world.entity.animal.Animal;
-import org.jetbrains.annotations.Nullable;
+public class DustBinCreature {
 
-public class DustBinCreature extends Animal {
-    @Nullable
-    @Override
-    public AgeableMob getBreedOffspring(ServerLevel p_146743_, AgeableMob p_146744_) {
-        return null;
-    }
 }
