dbec49ec7ec0a223003726061b7d5813578e1634
QuwackJ <quwackj@hotmail.com>
2023-03-20 18:07:02 -0700

    Creating Entities for our mod using the Animal capabilities of Minecraft. Following tutorial by YaBoiChips

--- src/main/java/io/github/quwackj/spiritedawayghs/entity/ModEntityTypes.java
+++ src/main/java/io/github/quwackj/spiritedawayghs/entity/ModEntityTypes.java
@@ -1,7 +1,26 @@
 package io.github.quwackj.spiritedawayghs.entity;
 
+import net.minecraft.server.level.ServerLevel;
+import net.minecraft.world.entity.AgeableMob;
+import net.minecraft.world.entity.EntityType;
+import net.minecraft.world.entity.animal.Animal;
+import net.minecraft.world.level.Level;
 import net.minecraftforge.registries.DeferredRegister;
+import org.jetbrains.annotations.Nullable;
 
-public class ModEntityTypes {
+public class ModEntityTypes extends Animal {
+    protected ModEntityTypes(EntityType<? extends Animal> animal, Level level) {
+        super(animal, level);
+    }
 
+    @Override
+    protected void registerGoals() {
+        super.registerGoals();
+    }
+
+    @Nullable
+    @Override
+    public AgeableMob getBreedOffspring(ServerLevel level, AgeableMob entity) {
+        return null;
+    }
 }
