a492e427248090fc8e365e060e13b9113d357982
QuwackJ <quwackj@hotmail.com>
2023-03-24 09:37:20 -0700

    Made code look cleaner

--- src/main/java/io/github/quwackj/spiritedawayghs/initializer/ModEntities.java
+++ src/main/java/io/github/quwackj/spiritedawayghs/initializer/ModEntities.java
@@ -1,7 +1,7 @@
 package io.github.quwackj.spiritedawayghs.initializer;
 
 import io.github.quwackj.spiritedawayghs.SpiritedAwayGHS;
-import io.github.quwackj.spiritedawayghs.entities.DustBinCreature;
+import io.github.quwackj.spiritedawayghs.entities.DustBunny;
 import net.minecraft.world.entity.EntityType;
 import net.minecraft.world.entity.MobCategory;
 import net.minecraftforge.registries.DeferredRegister;
@@ -11,6 +11,6 @@ import net.minecraftforge.registries.RegistryObject;
 public class ModEntities {
     public static final DeferredRegister<EntityType<?>> ENTITIES = DeferredRegister.create(ForgeRegistries.ENTITY_TYPES, SpiritedAwayGHS.MOD_ID);
 
-    public static final RegistryObject<EntityType<DustBinCreature>> MOD_ENTITY = ENTITIES.register("Dust Bin Creature",
-            () -> EntityType.Builder.of(DustBinCreature::new, MobCategory.CREATURE).build("Dust Bin Creature"));
+    public static final RegistryObject<EntityType<DustBunny>> MOD_ENTITY = ENTITIES.register("Dust Bunny",
+            () -> EntityType.Builder.of(DustBunny::new, MobCategory.CREATURE).build("Dust Bunny"));
 }
