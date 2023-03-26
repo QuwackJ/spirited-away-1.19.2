d2ad4906d329fa23992b636068b3885242e985dc
QuwackJ <quwackj@hotmail.com>
2023-03-25 22:44:36 -0700

    Another update to Soot model files

--- src/main/java/client/models/SootModel.java
+++ src/main/java/client/models/SootModel.java
@@ -19,17 +19,17 @@ import net.minecraft.resources.ResourceLocation;
 public class SootModel extends EntityModel<Soot> {
 	// This layer location should be baked with EntityRendererProvider.Context in the entity renderer and passed into this model's constructor
 	public static final ModelLayerLocation LAYER_LOCATION = new ModelLayerLocation(new ResourceLocation(SpiritedAwayGHS.MOD_ID, "soot"), "main");
-	private final ModelPart body;
+	private final ModelPart bone;
 
 	public SootModel(ModelPart root) {
-		this.body = root.getChild("body");
+		this.bone = root.getChild("bone");
 	}
 
 	public static LayerDefinition createBodyLayer() {
 		MeshDefinition meshdefinition = new MeshDefinition();
 		PartDefinition partdefinition = meshdefinition.getRoot();
 
-		PartDefinition body = partdefinition.addOrReplaceChild("body", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -16.0F, -8.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(0.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
+		PartDefinition bone = partdefinition.addOrReplaceChild("bone", CubeListBuilder.create().texOffs(0, 0).addBox(-8.0F, -10.0F, -8.0F, 16.0F, 16.0F, 16.0F, new CubeDeformation(-6.0F)), PartPose.offset(0.0F, 24.0F, 0.0F));
 
 		return LayerDefinition.create(meshdefinition, 64, 64);
 	}
@@ -41,6 +41,6 @@ public class SootModel extends EntityModel<Soot> {
 
 	@Override
 	public void renderToBuffer(PoseStack poseStack, VertexConsumer vertexConsumer, int packedLight, int packedOverlay, float red, float green, float blue, float alpha) {
-		body.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
+		bone.render(poseStack, vertexConsumer, packedLight, packedOverlay, red, green, blue, alpha);
 	}
 }
